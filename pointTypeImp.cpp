a4777ff99c88414170efad1268dd7e18f0392909
Justin Calma <calma.justin.00@gmail.com>
2020-10-12 10:42:43 -0700

    Implementation file for pointType class

--- pointTypeImp.cpp
+++ pointTypeImp.cpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include "pointType.h"
-
 using namespace std;
 
+// Setter for the x and y coordinate
 void pointType::setPoint(double x, double y) {
 	
 	xCoordinate = x;
@@ -12,24 +12,28 @@ void pointType::setPoint(double x, double y) {
 
 }
 
+// Print the x and y coordinates 
 void pointType::print() const {
 
 	cout << "(" << xCoordinate << ", " << yCoordinate << ")" << endl;
 
 }
 
+// Getter for the x coordinate
 double pointType::getX() const {
 
 	return xCoordinate;
 
 }
 
+// Getter for the y coordinate
 double pointType::getY() const {
 
 	return yCoordinate;
 
 }
 
+// Constructor for pointType object
 pointType::pointType(double x, double y) {
 
 	xCoordinate = x;
