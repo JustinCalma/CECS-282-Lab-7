ad609ed4cb5c0e77d4b5b45f1fa559f42fc92c1b
Justin Calma <calma.justin.00@gmail.com>
2020-10-12 10:39:26 -0700

    Header file for cylinderType class

--- cylinderType.h
+++ cylinderType.h
@@ -4,8 +4,10 @@
 #define H_CylinderType
 #include "circleType.h"
 
+// Create cylinderType class; Inherits circleType class
 class cylinderType: public circleType {
 	
+	// Delcare the public methods 
 	public:
 		void print() const;
 		
@@ -18,6 +20,7 @@ class cylinderType: public circleType {
 		double getSurfaceArea() const;
 		cylinderType(double x = 0.0, double y = 0.0, double r = 0.0, double h = 0.0);
 
+	// Declare the protected attribute
 	protected:
 		double height;
 
